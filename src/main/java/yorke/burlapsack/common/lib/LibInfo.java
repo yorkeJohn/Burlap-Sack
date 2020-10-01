0d829ff47ea4de9224cc495f73631c2caf6f4727
John Yorke <yorkejohn02@gmail.com>
2020-10-01 20:43:01 -0300

    Update version to 1.1

--- src/main/java/yorke/burlapsack/common/lib/LibInfo.java
+++ src/main/java/yorke/burlapsack/common/lib/LibInfo.java
@@ -4,5 +4,5 @@ public class LibInfo
 {
 	public static final String MODID = "burlapsack";
 	public static final String NAME = "Burlap Sack";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.1";
 }
